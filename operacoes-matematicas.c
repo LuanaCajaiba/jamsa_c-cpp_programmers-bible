2d4490218e4319eeaa8d3e002f2d282d94861518
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-11 23:22:01 -0300

    Atualização

---
+++ operacoes-matematicas.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    float pi = 3.14159;
+    float raio = 2.0031;
+
+    printf("A área do circulo é %e\n", 2 * pi * raio);
+    printf("A área do círculo %E\n", 2 * pi * raio);
+}
