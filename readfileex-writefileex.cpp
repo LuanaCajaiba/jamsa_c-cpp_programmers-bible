bb14f1301ce12a0ab524bbfedab44ecef68580b2
LuanaCajaiba <luana_cajaiba1@hotmail.com>
2021-04-12 01:41:29 -0300

    Atualização

---
+++ readfileex-writefileex.cpp
@@ -0,0 +1,9 @@
+Bool ReadFileEx(
+    HANDLE hArq,    //indicativo do arquivo a ler
+    LPVOID lpBuffer,    //endereço do buffer
+    DWORD nNumBytesALer,    // números de bytes a ler
+    LPOVERLAPPED lsSobreposta,  //endereço do deslocamento
+    LPOVERLAPPED_COMPLETION_ROUTINE
+    lpRotinaFinalizacao    //endereço da rotina de finalização
+
+);
